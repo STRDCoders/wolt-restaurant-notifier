fef0fcd1762cfc5012c4e940da117778af7697a0
Tomer Blecher <tomer.blecher.brolix@gmail.com>
2022-02-03 11:37:51 +0200

    Connect to mongo on init

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { LoggerFactory } from "./utils/logger-factory";
 import { Logger } from "winston";
+import { MongooseConnector } from "./services/MongooseConnector";
+import { Constants } from "./utils/constants";
 
 const logger: Logger = LoggerFactory.getLogger("main");
 
-function startup() {
+async function startup() {
   logger.info("App is starting...");
+  await MongooseConnector.connect(Constants.mongoUri);
 }
 
-startup();
+startup().then(() => logger.info("Initialized"));
